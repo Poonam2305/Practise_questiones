80a695007a8d91f59457310ee0dcaeec4502672c
ddeepak8894 <ddeepak8894@gmail.com>
2024-07-24 13:25:32 +0530

    done the changes

--- power_of_number.py
+++ power_of_number.py
@@ -1,9 +1,10 @@
-num,power = 3,4
-power_of_number = num **power
-print("the power is: ",power_of_number)
-
-
+num, power = 3, 4
+power_of_number = num ** power
+print("the power is: ", power_of_number)
 
 #by using power function:
-num,power = 2,3
-print(pow(num,power))
+num = int(input("provide number to find power: "))
+power= int(input("provide power : "))
+power_num = pow(num, power)
+print("the expected output is: ", power_num)
+
